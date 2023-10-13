3c32c0b567d7ae95ea155a4c9c32875856535a4a
Crowdbotics <team@crowdbotics.com>
2023-10-13 15:53:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fleet-management-ap-43928",
+  url: "https://fleet-management-ap-43928.botics.co",
+  api: "https://fleet-management-ap-43928.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
